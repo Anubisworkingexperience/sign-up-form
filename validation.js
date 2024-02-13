a747f3c94c8171c5a9f1e78613ecba3c67fdfc41
Anubisworkingexperience <79977978+Anubisworkingexperience@users.noreply.github.com>
2024-02-13 23:29:05 +0300

    Add password validation, confirm

--- validation.js
+++ validation.js
@@ -1,3 +1,24 @@
+// password validation:
+// Minimum eight characters,
+// at least one uppercase letter,
+// one lowercase letter,
+// one number and
+// one special character
+// --applied in html pattern
+
 // password confirmation
-password = document.querySelector('.pass')
-confirm = document.querySelector('.confirm')
+const password = document.querySelector('.pass');
+const confirm = document.querySelector('.confirm');
+const submit = document.querySelector('.submit');
+const message = document.querySelector('.message');
+
+submit.addEventListener('click', () => {
+    if (password.value != "" && confirm.value != "") {
+        if (password.value != confirm.value) {
+            confirm.setCustomValidity("Passwords don't match");
+        }
+        else {
+            confirm.setCustomValidity("")
+        }
+    }
+});
